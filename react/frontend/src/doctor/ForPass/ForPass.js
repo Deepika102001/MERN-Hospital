2f86df38ee01f82bd474f41b82d9bcf8337fb6f1
jagadeshwaran13 <64747687+jagadeshwaran13@users.noreply.github.com>
2024-01-01 12:29:33 +0530

    Update ForPass.js

--- react/frontend/src/doctor/ForPass/ForPass.js
+++ react/frontend/src/doctor/ForPass/ForPass.js
@@ -70,7 +70,7 @@ export const DoctorForgotPassword=()=>{
                 placeholder="Enter Password"
                 /><br></br><br></br>
                 {/* <Link to="/Doctorlogin"> */}
-                <button onClick={save}>save the details</button>
+                <button onClick={save}>Reset Password</button>
                 {/* </Link> */}
         {err?<p className="error">{err}</p>:null}
 
